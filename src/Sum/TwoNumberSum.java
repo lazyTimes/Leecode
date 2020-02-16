3d69b83811a4efaae30d9ce91360ac33825c65bf
lazyTimes <1097483508@qq.com>
2020-02-16 13:24:12 +0800

    Leecode求两数之和

---
+++ src/Sum/TwoNumberSum.java
@@ -0,0 +1,69 @@
+package Sum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @program: Leecode
+ * @description: 两数之和
+ * @author: zhaoxudong
+ * @create: 2020-02-16 10:41
+ **/
+public class TwoNumberSum {
+
+    /**
+     * 第一种方式：使用最简单的暴力破解法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+
+    }
+
+    /**
+     * 使用HashMap实现
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new TwoNumberSum().twoSum2(new int[]{1, 2, 3, 4, 7, 8, 9}, 9);
+        System.err.println(Arrays.toString(ints));
+    }
+}
